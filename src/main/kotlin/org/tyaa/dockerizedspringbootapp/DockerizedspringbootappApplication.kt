398d665672dd0419a28d3c19c0f53f90da3f99e4
YuriiTrofimenko <tyaa@ukr.net>
2020-01-05 17:22:50 +0200

    create controller

--- src/main/kotlin/org/tyaa/dockerizedspringbootapp/DockerizedspringbootappApplication.kt
+++ src/main/kotlin/org/tyaa/dockerizedspringbootapp/DockerizedspringbootappApplication.kt
@@ -2,9 +2,17 @@ package org.tyaa.dockerizedspringbootapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DockerizedspringbootappApplication
+
+@RestController
+class GreetingsController {
+	@GetMapping("/greetings")
+	fun greetings() = "hello from a Docker"
+}
 
 fun main(args: Array<String>) {
 	runApplication<DockerizedspringbootappApplication>(*args)
